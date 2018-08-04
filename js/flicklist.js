3abb9e7beaed2cb4788f58a45fa757633761df1e
Brian Schiller <bgschiller@gmail.com>
2018-08-04 00:48:49 -0600

    Move from jquery to Vue

--- js/flicklist.js
+++ js/flicklist.js
@@ -1,114 +1,55 @@
 
-
-$(document).ready(function() {
-  discoverMovies(render);
-});
-
-
-
-var model = {
-  watchlistItems: [],
-  browseItems: []
-}
-
-
 var api = {
   root: "https://api.themoviedb.org/3",
-  token: "8e888fa39ec243e662e1fb738c42ae99" // TODO 0 add your api key
+  token: "f6bd21e39046f6a08df40d82ad8cffd3"
 }
 
+var flicklistView = new Vue({
+	el: '#mount-target',
+	data: function() {
+		return {
+			// This is the data model.
+			// Whenever it changes, Vue will automatically re-render
+			// the html for us.
+			watchlistItems: [],
+      browseItems: [],
+      // TODO 8B
+		};
+	},
+	methods: {
+		discoverMovies: function () {
+			/**
+			 * Makes an AJAX request to themoviedb.org, asking for some movies
+			 * if successful, updates the data.browseItems appropriately
+			 */
 
-/**
- * Makes an AJAX request to /discover/movie endpoint of the API
- *
- * if successful, updates the model.browseItems appropriately, and then invokes
- * the callback function that was passed in
- */
-function discoverMovies(callback) {
-  $.ajax({
-    url: api.root + "/discover/movie",
-    data: {
-      api_key: api.token
+			fetch(`${api.root}/discover/movie?api_key=${api.token}`)
+					.then(resp => resp.ok ? resp.json() : Promise.reject(resp))
+					.then((response) => {
+						console.log("We got a response from The Movie DB!");
+						console.log(response);
+
+						this.browseItems = response.results;
+
+					});
     },
-    success: function(response) {
-      model.browseItems = response.results;
-      callback();
-    }
-  });
-}
+    searchMovies: function(searchTerm) {
+      // Make an AJAX request to the /search/movie endpoint
+      // of the API, using the query string that was passed in.
+      //
+      // if successful, update this.browseItems appropriately.
+      // This update will automatically trigger a re-render.
+      console.log(`searching for movies with "${searchTerm}" in their title...`);
 
-
-/**
- * Makes an AJAX request to the /search/movie endpoint of the API, using the 
- * query string that was passed in
- *
- * if successful, updates model.browseItems appropriately and then invokes
- * the callback function that was passed in
- */
-function searchMovies(searchTerm, callback) {
-  console.log("searching for movies with '" + searchTerm + "' in their title...");
-
-  // TODO 9
-  // implement this function as described in the comment above
-  // you can use the body of discoverMovies as a jumping off point
-
-
-}
-
-
-/**
- * re-renders the page with new content, based on the current state of the model
- */
-function render() {
-
-  // clear everything
-  $("#section-watchlist ul").empty();
-  $("#section-browse ul").empty();
-
-  // insert watchlist items
-  model.watchlistItems.forEach(function(movie) {
-    var title = $("<p></p>").text(movie.original_title);
-    var itemView = $("<li></li>")
-      .append(title)
-      // TODO 3
-      // give itemView a class attribute of "item-watchlist"
-
-    $("#section-watchlist ul").append(itemView);
-  });
-
-  // insert browse items
-  model.browseItems.forEach(function(movie) {
-    var title = $("<h4></h4>").text(movie.original_title);
-    var button = $("<button></button>")
-      .text("Add to Watchlist")
-      .click(function() {
-        model.watchlistItems.push(movie);
-        render();
-      });
-      // TODO 2
-      // the button should be disabled if this movie is already in
-      // the user's watchlist
-      // see jQuery .prop() and Array.indexOf()
-
-
-    // TODO 1
-    // create a paragraph containing the movie object's .overview value
-    // then, in the code block below,
-    // append the paragraph in between the title and the button
-
-
-    // append everything to itemView, along with an <hr/>
-    var itemView = $("<li></li>")
-      .append($("<hr/>"))
-      .append(title)
-      .append(button);
-
-    // append the itemView to the list
-    $("#section-browse ul").append(itemView);
-  });
-  
-}
-
-
-
-
+      // TODO 9
+      // implement this function as described in the comment above
+      // you can use the body of discoverMovies as a jumping off point
+    },
+		addToWatchlist: function(movie) {
+			this.watchlistItems.push(movie);
+		},
+	},
+	mounted: function () {
+		this.discoverMovies();
+	},
+});
