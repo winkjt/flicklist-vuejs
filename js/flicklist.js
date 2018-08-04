3952fef0e96fcacf6ea5604e8e6dbac24870133a
Brian Schiller <bgschiller@gmail.com>
2018-08-04 01:39:38 -0600

    update from jquery to vue

--- js/flicklist.js
+++ js/flicklist.js
@@ -1,139 +1,68 @@
 
-
-var model = {
-  watchlistItems: [],
-  browseItems: []
-}
-
-
 var api = {
-
   root: "https://api.themoviedb.org/3",
-  token: "TODO", // TODO 0 add your api key
-
-  /**
-   * Given a movie object, returns the url to its poster image
-   */
-  posterUrl: function(movie) {
-    // TODO 4b
-    // implement this function
-
-    return "http://images5.fanpop.com/image/photos/25100000/movie-poster-rapunzel-and-eugene-25184488-300-450.jpg" 
-  }
+  token: "f6bd21e39046f6a08df40d82ad8cffd3"
 }
 
+var flicklistView = new Vue({
+	el: '#mount-target',
+	data: function() {
+		return {
+			// This is the data model.
+			// Whenever it changes, Vue will automatically re-render
+			// the html for us.
+			watchlistItems: [],
+      browseItems: [],
+      searchTerm: null,
+		};
+	},
+	methods: {
+		discoverMovies: function () {
+			/**
+			 * Makes an AJAX request to themoviedb.org, asking for some movies
+			 * if successful, updates the data.browseItems appropriately
+			 */
 
-/**
- * Makes an AJAX request to themoviedb.org, asking for some movies
- * if successful, updates the model.browseItems appropriately, and then invokes
- * the callback function that was passed in
- */
-function discoverMovies(callback) {
-  $.ajax({
-    url: api.root + "/discover/movie",
-    data: {
-      api_key: api.token
+			fetch(`${api.root}/discover/movie?api_key=${api.token}`)
+					.then(resp => resp.ok ? resp.json() : Promise.reject(resp))
+					.then((response) => {
+						console.log("We got a response from The Movie DB!");
+						console.log(response);
+
+						this.browseItems = response.results;
+
+					});
     },
-    success: function(response) {
-      model.browseItems = response.results;
-      callback(response);
-      console.log(response);
-    }
-  });
-}
+    searchMovies: function(searchTerm) {
+      // Make an AJAX request to the /search/movie endpoint
+      // of the API, using the query string that was passed in.
+      //
+      // if successful, update this.browseItems appropriately.
+      // This update will automatically trigger a re-render.
+      console.log(`searching for movies with "${searchTerm}" in their title...`);
 
+      fetch(`${api.root}/search/movie?api_key=${api.token}&query=${searchTerm}`)
+      .then(resp => resp.ok ? resp.json() : Promise.reject(resp))
+      .then((response) => {
+        console.log("We got a response from The Movie DB!");
+        console.log(response);
 
-/**
- * Makes an AJAX request to the /search endpoint of the API, using the 
- * query string that was passed in
- *
- * if successful, updates model.browseItems appropriately and then invokes
- * the callback function that was passed in
- */
-function searchMovies(query, callback) {
-  $.ajax({
-    url: api.root + "/search/movie",
-    data: {
-      api_key: api.token,
-      query: query
+        this.browseItems = response.results;
+
+      });
+
     },
-    success: function(response) {
-      model.browseItems = response.results;
-      callback(response);
-    }
-  });
-}
+    posterUrl: function(movie) {
+      // TODO 4b
+      // implement this function
 
-
-/**
- * re-renders the page with new content, based on the current state of the model
- */
-function render() {
-
-  // clear everything
-  $("#section-watchlist ul").empty();
-  $("#section-browse ul").empty();
-
-  // insert watchlist items
-  model.watchlistItems.forEach(function(movie) {
-    var title = $("<h6></h6>").text(movie.original_title);
-
-    // TODO 1 
-    // add an "I watched it" button and append it below the title
-    // Clicking should remove this movie from the watchlist and re-render
-
-    // TODO 2i
-    // apply the classes "btn btn-danger" to the "I watched it button"
-
-    // TODO 4a
-    // add a poster image and append it inside the 
-    // panel body above the button
-
-    // TODO 2g
-    // re-implement the li as a bootstrap panel with a heading and a body
-    var itemView = $("<li></li>")
-      .append(title)
-      .attr("class", "item-watchlist");
-
-    $("#section-watchlist ul").append(itemView);
-  });
-
-  // insert browse items
-  model.browseItems.forEach(function(movie) {
-
-    // TODO 2d continued
-    // style this list item to look like the demo
-    // You'll also need to make changes in index.html.
-    // use the following BS classes:
-    // "list-group", "list-group-item", btn", "btn-primary", 
-
-    var title = $("<h4></h4>").text(movie.original_title);
-
-    var button = $("<button></button>")
-      .text("Add to Watchlist")
-      .click(function() {
-        model.watchlistItems.push(movie);
-        render();
-      })
-      .prop("disabled", model.watchlistItems.indexOf(movie) !== -1);
-
-    var overview = $("<p></p>").text(movie.overview);
-
-    // append everything to itemView, along with an <hr/>
-    var itemView = $("<li></li>")
-      .append(title)
-      .append(overview)
-      .append(button);
-      
-    // append the itemView to the list
-    $("#section-browse ul").append(itemView);
-  });
-  
-}
-
-
-// When the HTML document is ready, we call the discoverMovies function,
-// and pass the render function as its callback
-$(document).ready(function() {
-  discoverMovies(render);
+      return "http://images5.fanpop.com/image/photos/25100000/movie-poster-rapunzel-and-eugene-25184488-300-450.jpg"
+    },
+		addToWatchlist: function(movie) {
+			this.watchlistItems.push(movie);
+		},
+	},
+	mounted: function () {
+		this.discoverMovies();
+	},
 });
